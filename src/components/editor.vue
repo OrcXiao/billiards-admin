d6bc1b3f526e332e435f3e44e0540076dd3100ef
xiaojianqiang <471951913@qq.com>
2020-05-28 21:40:43 +0800

    0528,xiao, 增加资讯的富文本

--- src/components/editor.vue
+++ src/components/editor.vue
@@ -52,7 +52,7 @@
       seteditor() {
         this.editor = new Editor(this.$refs.toolbar, this.$refs.editor)
         this.editor.customConfig.uploadImgShowBase64 = false // base 64 存储图片
-        this.editor.customConfig.uploadImgServer = 'http://otp.cdinfotech.top/file/upload_images'// 配置服务器端地址
+        this.editor.customConfig.uploadImgServer = ''// 配置服务器端地址
         this.editor.customConfig.uploadImgHeaders = { }// 自定义 header
         this.editor.customConfig.uploadFileName = 'file' // 后端接受上传文件的参数名
         this.editor.customConfig.uploadImgMaxSize = 2 * 1024 * 1024 // 将图片大小限制为 2M
