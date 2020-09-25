9bee6171b88eb43e86a9e4ddebd5d813f7a8cd14
OrcXiao <471951913@qq.com>
2020-09-25 17:41:56 +0800

    0925,xiao,

--- src/axios/index.js
+++ src/axios/index.js
@@ -5,14 +5,16 @@ axios.defaults.timeout = 50000;
 
 //请求的基础url
 // axios.defaults.baseURL = 'http://www.taiqiuplus.com:8082/api';
+
 // axios.defaults.baseURL = 'http://billiards.utools.club/api';
-axios.defaults.baseURL = 'http://billiards.aafu.xyz:6017/';
+
+axios.defaults.baseURL = 'http://www.taiqiuplus.com:6017';
 
 axios.interceptors.response.use(res => {
-  //错误码为-1, 统一进行提示.
-  if (res.data.resultCode !== 0) {
-    Message.error(res.data.resultMsg || '系统错误');
-  }
+  // //错误码为-1, 统一进行提示.
+  // if (res.data.resultCode !== 0) {
+  //   Message.error(res.data.resultMsg || '系统错误');
+  // }
   return res
 });
 
